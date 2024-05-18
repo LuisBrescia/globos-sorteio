44201929776178643d44118562b9b5278e7a08db
LuisBrescia <luisfelipebrescia@gmail.com>
2024-05-18 01:03:48 -0300

    Bolas aleatorias e texturas adicionadas

--- src/scripts/model-loader.js
+++ src/scripts/model-loader.js
@@ -6,6 +6,29 @@ import { audioLoader, listener } from './audio-loader.js';
 const textureLoader = new THREE.TextureLoader();
 export let actions = [];
 
+// Numeros de 0 a 9
+// const order = [
+//   [1, 0, 2, 3, 4, 5, 6, 7, 8, 9],
+//   [1, 0, 2, 3, 4, 5, 6, 7, 8, 9],
+//   [1, 0, 2, 3, 4, 5, 6, 7, 8, 9],
+//   [1, 0, 2, 3, 4, 5, 6, 7, 8, 9],
+// ];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const order = [];
+
+for (let i = 0; i < 4; i++) { // assumindo que você quer 4 vetores
+  const array = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  order.push(array);
+}
+
 export function loadModel() {
   const loader = new GLTFLoader();
   loader.load('4Globe.glb', (gltf) => {
@@ -13,17 +36,27 @@ export function loadModel() {
     model.position.set(0, -2, 0);
 
     model.traverse((node) => {
+
       if (node.isMesh && node.name.match(/^Ball\d+$/)) {
         node.castShadow = true;
         node.receiveShadow = true;
-        const ballNumber = parseInt(node.name.replace('Ball', ''), 10);
-        const textureNumber = ballNumber % 10;
-
+        const ballNumber = node.name.replace('Ball', '');
+        const textureNumber = order[parseInt(ballNumber[0], 10) - 1][parseInt(ballNumber[1], 10)];
         const texture = textureLoader.load(`Textures/Ball_${textureNumber}.png`);
         texture.flipY = false;
-
         node.material = new THREE.MeshStandardMaterial({ map: texture });
+      } else if (node.isMesh && node.name.startsWith('Base')) {
+        console.log(node.name);
+        const texture = textureLoader.load(`Textures/Wood.jpg`);
+        node.material = new THREE.MeshStandardMaterial({ map: texture });
+      } else if (node.isMesh && node.name.startsWith('Sphere')) {
+        const texture = textureLoader.load(`Textures/Globe.jpg`);
+        node.material = new THREE.MeshStandardMaterial({ map: texture });
+      } else if (!node.isMesh) {
+        console.log("NOT MESH");
       }
+
+      // Globe tube parte de baixo // preto
     });
 
     scene.add(model);
--- src/scripts/scene-setup.js
+++ src/scripts/scene-setup.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 export let scene, camera, renderer, controls;
 export const mixers = [];
 
-const MACRO_CONTROLS = true; // Ativar controles de órbita
+const MACRO_CONTROLS = false; // Ativar controles de órbita
 
 export function initScene() {
   scene = new THREE.Scene();
@@ -18,7 +18,7 @@ export function initScene() {
   renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
 
-  controls = new OrbitControls(camera, renderer.domElement);
+  controls = MACRO_CONTROLS ? new OrbitControls(camera, renderer.domElement) : null;
   if (MACRO_CONTROLS) {
     controls.enableDamping = true;
     controls.dampingFactor = 0.25;
@@ -26,17 +26,25 @@ export function initScene() {
   }
 
   // Adicionar luzes
-  const hemisphereLight = new THREE.HemisphereLight(0xFFFFCC, 0x444444, 1);
-  hemisphereLight.position.set(0, 200, 0);
-  scene.add(hemisphereLight);
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
+  ambientLight.position.set(0, 0, 0);
+  scene.add(ambientLight);
 
-  const directionalLightFrente = new THREE.DirectionalLight(0xFFFF00, 2);
-  directionalLightFrente.position.set(0, 0, 10).normalize();
-  scene.add(directionalLightFrente);
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+  directionalLight.position.set(0, -1, 2);
+  scene.add(directionalLight);
 
-  const directionalLightCima = new THREE.DirectionalLight(0xFFFF00, 2);
-  directionalLightCima.position.set(0, 10, 0).normalize();
-  scene.add(directionalLightCima);
+  // const hemisphereLight = new THREE.HemisphereLight(0xFFFFFF, 0x444444, .5);
+  // hemisphereLight.position.set(0, 200, 0);
+  // scene.add(hemisphereLight);
+
+  // const directionalLightFrente = new THREE.DirectionalLight(0xFFFFFF, .5);
+  // directionalLightFrente.position.set(10, 0, 10).normalize();
+  // scene.add(directionalLightFrente);
+
+  // const directionalLightCima = new THREE.DirectionalLight(0xFFFFFF, .5);
+  // directionalLightCima.position.set(-10, 10, 0).normalize();
+  // scene.add(directionalLightCima);
 }
 
 const clock = new THREE.Clock();
