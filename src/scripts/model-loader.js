8bf37aeb6d354f2ac3c1958912bb006ad0e8a0da
LuisBrescia <luisfelipebrescia@gmail.com>
2024-05-27 11:13:17 -0300

    Caminhos absolutos, escolha de numeros e quantidades de bolas a sairem

--- src/scripts/model-loader.js
+++ src/scripts/model-loader.js
@@ -1,40 +1,26 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { mixers, scene, renderer, camera } from './scene-setup.js';
+import { predefinedOrder } from '@/utils/predefinedOrder.js';
 
 const textureLoader = new THREE.TextureLoader();
 export let actions = [];
 
-// const order = [
-//   [5, 0, 7, 8, 1, 2, 9, 4, 3, 6],
-//   [7, 5, 6, 2, 3, 0, 1, 8, 9, 4],
-//   [2, 9, 6, 0, 4, 5, 3, 7, 1, 8],
-//   [3, 9, 2, 4, 1, 5, 6, 7, 8, 0],
-// ];
+// Constantes para a quantidade de números e globos
+const QUANTIDADE_NUMEROS = predefinedOrder.length;
+const QUANTIDADE_GLOBOS = predefinedOrder[0].length;
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const order = [];
-for (let i = 0; i < 4; i++) {
-  const array = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  order.push(array);
-}
-
-const orderLocalStorage = Array.from({ length: 10 }, () => []);
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 4; j++) {
-    orderLocalStorage[i].push(order[j][i]);
+// Salva a ordem dos números em localStorage
+const orderLocalStorage = Array.from({ length: QUANTIDADE_NUMEROS }, () => []);
+for (let i = 0; i < QUANTIDADE_NUMEROS; i++) {
+  for (let j = 0; j < QUANTIDADE_GLOBOS; j++) {
+    orderLocalStorage[i].push(predefinedOrder[i][j]);
   }
 }
 console.log(orderLocalStorage);
 localStorage.setItem('GloboSorteioOrdem', JSON.stringify(orderLocalStorage));
 
+// Função para carregar o modelo
 export function loadModel() {
   const loader = new GLTFLoader();
   loader.load('4Globe.glb', (gltf) => {
@@ -44,13 +30,20 @@ export function loadModel() {
     model.traverse((node) => {
       if (node.name.match(/^Ball\d+$/)) {
         const ballNumber = node.name.replace('Ball', '');
-        const textureNumber = order[parseInt(ballNumber[0], 10) - 1][parseInt(ballNumber[1], 10)];
-        const texture = textureLoader.load(`Textures/Ball_${textureNumber}.png`);
-        texture.flipY = false;
-        // inverter cores da textura
-        node.material = new THREE.MeshStandardMaterial({ map: texture });
-        node.material.metalness = 0;
-        node.material.roughness = 1;
+        const globoIndex = parseInt(ballNumber[0], 10) - 1;
+        const ballIndex = parseInt(ballNumber[1], 10);
+
+        // Verifica se o número da bola é maior que a quantidade de bolas
+        if (ballIndex >= QUANTIDADE_NUMEROS) {
+          node.visible = false;
+        } else {
+          const textureNumber = predefinedOrder[ballIndex][globoIndex];
+          const texture = textureLoader.load(`Textures/Ball_${textureNumber}.png`);
+          texture.flipY = false;
+          node.material = new THREE.MeshStandardMaterial({ map: texture });
+          node.material.metalness = 0;
+          node.material.roughness = 1;
+        }
       } else if (node.isMesh) {
         node.material.metalness = 1;
         node.material.roughness = 0.25;
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      '@models': path.resolve(__dirname, 'src/assets/3d_models'),
-      '@textures': path.resolve(__dirname, 'src/assets/textures'),
-      '@audios': path.resolve(__dirname, 'src/assets/audios'),
+      '@': path.resolve(__dirname, './src')
     },
   },
   build: {
