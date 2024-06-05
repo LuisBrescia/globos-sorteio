a8986c2b5bbd9f90e8bc480867516d83fe27dfdf
LuisBrescia <luisfelipebrescia@gmail.com>
2024-06-05 19:03:45 -0300

    Dialog e input adicionado

--- src/main.js
+++ src/main.js
@@ -12,6 +12,27 @@ document.getElementById('restartButton').addEventListener('click', restartAnimat
 
 // Da para so colocar no body ou componente pai tambem
 document.getElementById('menuButton').addEventListener('click', () => {
-  document.getElementById('menuContent').classList.toggle('active');
-  document.getElementById('menuButton').classList.toggle('active');
+  document.getElementById('menuWrapper').classList.toggle('active');
+});
+
+// ao clicar fora da tela fora do menuwrapper ele fecha
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('#menuWrapper') && !event.target.closest('#menuButton')) {
+    document.getElementById('menuWrapper').classList.remove('active');
+  }
+});
+
+document.getElementById('chooseOrder').addEventListener('click', () => {
+  document.getElementById('modal').classList.add('active');
+  document.getElementById('menuWrapper').classList.remove('active');
+});
+
+document.getElementById('closeDialog').addEventListener('click', () => {
+  document.getElementById('modal').classList.remove('active');
+});
+
+document.getElementById('saveOrder').addEventListener('click', () => {
+  const order = document.getElementById('orderInput').value;
+  localStorage.setItem('GloboSorteioOrdem', order);
+  document.getElementById('modal').classList.remove('active');
 });
