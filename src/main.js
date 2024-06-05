ca9b2b2b55474aeae70676b84b7519197161be0b
LuisBrescia <luisfelipebrescia@gmail.com>
2024-06-05 14:29:12 -0300

    Adicionado menu para controller

--- src/main.js
+++ src/main.js
@@ -9,3 +9,8 @@ loadModel();
 document.getElementById('startButton').addEventListener('click', startAnimation);
 document.getElementById('pauseButton').addEventListener('click', togglePauseAnimation);
 document.getElementById('restartButton').addEventListener('click', restartAnimation);
+
+document.getElementById('menuButton').addEventListener('click', () => {
+  document.getElementById('menuContent').classList.toggle('active');
+  document.getElementById('menuButton').classList.toggle('active');
+});
