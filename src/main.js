f7c519a91d1b1fa8e7b564ea122bfd2e87b3d751
LuisBrescia <luisfelipebrescia@gmail.com>
2024-06-05 14:38:37 -0300

    Novo modelo

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ document.getElementById('startButton').addEventListener('click', startAnimation)
 document.getElementById('pauseButton').addEventListener('click', togglePauseAnimation);
 document.getElementById('restartButton').addEventListener('click', restartAnimation);
 
+// Da para so colocar no body ou componente pai tambem
 document.getElementById('menuButton').addEventListener('click', () => {
   document.getElementById('menuContent').classList.toggle('active');
   document.getElementById('menuButton').classList.toggle('active');
