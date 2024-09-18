db6cae4c4a53008658d3aa6552daf280656b866f
luis.brescia <luis.brescia@atendeai.com>
2024-09-17 23:28:18 -0300

    fix: agora e realizado uma verificacao no tamanho de cada vetor para antes de renderizar textura das bolas

--- src/main.js
+++ src/main.js
@@ -1,30 +1,43 @@
-import { toggleStateAnimation, restartAnimation } from './scripts/animation-controls.js';
-import { toggleMenu, changeTheme, bodyCloseMenu, openModal, closeModal } from './scripts/menu-controls.js';
+import {
+  toggleStateAnimation,
+  restartAnimation,
+} from "./scripts/animation-controls.js";
+import {
+  toggleMenu,
+  changeTheme,
+  bodyCloseMenu,
+  openModal,
+  closeModal,
+} from "./scripts/menu-controls.js";
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const { initScene, animate } = await import('./scripts/scene-setup.js');
-  const { loadModel } = await import('./scripts/model-loader.js');
+document.addEventListener("DOMContentLoaded", async () => {
+  const { initScene, animate } = await import("./scripts/scene-setup.js");
+  const { loadModel } = await import("./scripts/model-loader.js");
+
   initScene();
   animate();
-  loadModel();
+  loadModel(predefinedOrder);
 });
 
-const predefinedOrder = JSON.parse(localStorage.getItem('GloboSorteioOrdem')) || Array.from({ length: 10 }, () => []);
+const predefinedOrder =
+  (await JSON.parse(localStorage.getItem("GloboSorteioOrdem"))) ||
+  Array.from({ length: 10 }, () => []);
+
 let currentRound = 0;
 
 function createNumberButtons(globoId) {
   const container = document.getElementById(globoId);
   for (let i = 0; i <= 9; i++) {
-    const button = document.createElement('button');
-    button.classList.add('btn-number');
+    const button = document.createElement("button");
+    button.classList.add("btn-number");
     button.textContent = i;
-    button.addEventListener('click', () => selectNumber(globoId, i));
+    button.addEventListener("click", () => selectNumber(globoId, i));
     container.appendChild(button);
   }
 }
 
 function selectNumber(globoId, number) {
-  const globoIndex = parseInt(globoId.replace('globo', '')) - 1;
+  const globoIndex = parseInt(globoId.replace("globo", "")) - 1;
   const currentSelection = predefinedOrder[currentRound][globoIndex];
 
   if (currentSelection === number) {
@@ -39,66 +52,81 @@ function selectNumber(globoId, number) {
   }
 
   updateButtons();
-  console.log('Predefined Order:', predefinedOrder);
+  console.log("Predefined Order:", predefinedOrder);
 }
 function updateButtons() {
-  document.querySelectorAll('.button-section').forEach((container, globoIndex) => {
-    const buttons = container.querySelectorAll('.btn-number');
-    buttons.forEach(button => {
-      const number = parseInt(button.textContent);
-      if (predefinedOrder[currentRound][globoIndex] === number) {
-        button.classList.add('selected');
-      } else {
-        button.classList.remove('selected');
-      }
+  document
+    .querySelectorAll(".button-section")
+    .forEach((container, globoIndex) => {
+      const buttons = container.querySelectorAll(".btn-number");
+      buttons.forEach((button) => {
+        const number = parseInt(button.textContent);
+        if (predefinedOrder[currentRound][globoIndex] === number) {
+          button.classList.add("selected");
+        } else {
+          button.classList.remove("selected");
+        }
 
-      if (isNumberUsedInGlobo(globoIndex, number)) {
-        button.classList.add('disabled');
-      } else {
-        button.classList.remove('disabled');
-      }
+        if (isNumberUsedInGlobo(globoIndex, number)) {
+          button.classList.add("disabled");
+        } else {
+          button.classList.remove("disabled");
+        }
+      });
     });
-  });
 }
 
 function isNumberUsedInGlobo(globoIndex, number) {
-  return predefinedOrder.some((round, roundIndex) => roundIndex !== currentRound && round[globoIndex] === number);
+  return predefinedOrder.some(
+    (round, roundIndex) =>
+      roundIndex !== currentRound && round[globoIndex] === number
+  );
 }
 
-document.getElementById('prevRound').addEventListener('click', () => {
+document.getElementById("prevRound").addEventListener("click", () => {
   if (currentRound > 0) {
     currentRound--;
-    document.getElementById('roundDisplay').textContent = `Rodada ${currentRound + 1}`;
+    document.getElementById("roundDisplay").textContent = `Rodada ${
+      currentRound + 1
+    }`;
     updateButtons();
   }
 });
 
-document.getElementById('nextRound').addEventListener('click', () => {
+document.getElementById("nextRound").addEventListener("click", () => {
   if (currentRound < 9) {
     currentRound++;
-    document.getElementById('roundDisplay').textContent = `Rodada ${currentRound + 1}`;
+    document.getElementById("roundDisplay").textContent = `Rodada ${
+      currentRound + 1
+    }`;
     updateButtons();
   }
 });
 
-createNumberButtons('globo1');
-createNumberButtons('globo2');
-createNumberButtons('globo3');
-createNumberButtons('globo4');
+createNumberButtons("globo1");
+createNumberButtons("globo2");
+createNumberButtons("globo3");
+createNumberButtons("globo4");
 
-document.getElementById('menuButton').addEventListener('click', toggleMenu);
-document.getElementById('changeThemeButton').addEventListener('click', changeTheme);
-document.getElementById('stateButton').addEventListener('click', toggleStateAnimation);
-document.getElementById('restartButton').addEventListener('click', restartAnimation);
-document.getElementById('customButton').addEventListener('click', openModal);
+document.getElementById("menuButton").addEventListener("click", toggleMenu);
+document
+  .getElementById("changeThemeButton")
+  .addEventListener("click", changeTheme);
+document
+  .getElementById("stateButton")
+  .addEventListener("click", toggleStateAnimation);
+document
+  .getElementById("restartButton")
+  .addEventListener("click", restartAnimation);
+document.getElementById("customButton").addEventListener("click", openModal);
 
-document.addEventListener('click', bodyCloseMenu);
-document.getElementById('closeModal').addEventListener('click', closeModal);
+document.addEventListener("click", bodyCloseMenu);
+document.getElementById("closeModal").addEventListener("click", closeModal);
 
-document.getElementById('saveOrder').addEventListener('click', () => {
-  localStorage.setItem('GloboSorteioOrdem', JSON.stringify(predefinedOrder));
+document.getElementById("saveOrder").addEventListener("click", () => {
+  localStorage.setItem("GloboSorteioOrdem", JSON.stringify(predefinedOrder));
   window.location.reload();
-  document.getElementById('modal').classList.remove('active');
+  document.getElementById("modal").classList.remove("active");
 });
 
 updateButtons();
--- src/scripts/model-loader.js
+++ src/scripts/model-loader.js
@@ -1,24 +1,22 @@
-import * as THREE from 'three';
-import { mixers, scene, renderer, camera } from './scene-setup.js';
-import { predefinedOrder } from '@/config/predefinedOrder.js';
+import * as THREE from "three";
+import { mixers, scene, renderer, camera } from "./scene-setup.js";
 
 const textureLoader = new THREE.TextureLoader();
 export let actions = [];
 
-// Pegar predefinedOrder do localStorage
-const sorteioOrder = JSON.parse(localStorage.getItem('GloboSorteioOrdem')) || predefinedOrder;
-
-export async function loadModel() {
-  const { GLTFLoader } = await import('three/examples/jsm/loaders/GLTFLoader');
-  const { DRACOLoader } = await import('three/examples/jsm/loaders/DRACOLoader');
+export async function loadModel(sorteioOrder) {
+  const { GLTFLoader } = await import("three/examples/jsm/loaders/GLTFLoader");
+  const { DRACOLoader } = await import(
+    "three/examples/jsm/loaders/DRACOLoader"
+  );
   const loader = new GLTFLoader();
   const dracoLoader = new DRACOLoader();
-  dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+  dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
   loader.setDRACOLoader(dracoLoader);
 
   // Promises para carregar o modelo GLB
   const modelPromise = new Promise((resolve, reject) => {
-    loader.load('4Globe-draco.glb', resolve, undefined, reject);
+    loader.load("4Globe-draco.glb", resolve, undefined, reject);
   });
 
   // Promises para carregar texturas
@@ -27,61 +25,77 @@ export async function loadModel() {
     order.forEach((textureNumber) => {
       texturePromises.push(
         new Promise((resolve, reject) => {
-          textureLoader.load(`Textures/Ball_${textureNumber}.png`, resolve, undefined, reject);
+          textureLoader.load(
+            `Textures/Ball_${textureNumber}.png`,
+            resolve,
+            undefined,
+            reject
+          );
         })
       );
     });
   });
 
   // Carregar modelo e texturas em paralelo
-  Promise.all([modelPromise, ...texturePromises]).then(([gltf, ...textures]) => {
-    const model = gltf.scene;
-    model.position.set(0, -2, 0);
+  Promise.all([modelPromise, ...texturePromises])
+    .then(([gltf, ...textures]) => {
+      const model = gltf.scene;
+      model.position.set(0, -2, 0);
 
-    model.traverse((node) => {
-      if (node.name.match(/^Ball\d+$/)) {
-        const ballNumber = node.name.replace('Ball', '');
-        const globoIndex = parseInt(ballNumber[0], 10) - 1;
-        const ballIndex = parseInt(ballNumber[1], 10);
+      model.traverse((node) => {
+        if (node.name.match(/^Ball\d+$/)) {
+          const ballNumber = node.name.replace("Ball", "");
+          const globoIndex = parseInt(ballNumber[0], 10) - 1;
+          const ballIndex = parseInt(ballNumber[1], 10);
 
-        // Verifica se o número da bola é maior que a quantidade de bolas
-        if (ballIndex >= sorteioOrder.length) {
-          node.visible = false;
+          let position = 0;
+          for (let i = 0; i < sorteioOrder.length; i++) {
+            if (sorteioOrder[i].length === 0) {
+              position = i;
+              break;
+            }
+          }
+
+          if (ballIndex >= position) {
+            node.visible = false;
+          } else {
+            const textureNumber = sorteioOrder[ballIndex][globoIndex];
+            const texture = textures.find((tex) =>
+              tex.image.src.includes(`Ball_${textureNumber}.png`)
+            );
+            texture.flipY = false;
+            node.material = new THREE.MeshStandardMaterial({ map: texture });
+            node.material.metalness = 0;
+            node.material.roughness = 1;
+            node.castShadow = false;
+            node.receiveShadow = false;
+            node.material.color = new THREE.Color(0xffffff);
+            node.material.transparent = true;
+          }
+        } else if (node.isMesh) {
+          node.material.metalness = 1;
+          node.material.roughness = 0.25;
+          console.log("MESH", node.name);
         } else {
-          const textureNumber = sorteioOrder[ballIndex][globoIndex];
-          const texture = textures.find((tex) => tex.image.src.includes(`Ball_${textureNumber}.png`));
-          texture.flipY = false;
-          node.material = new THREE.MeshStandardMaterial({ map: texture });
-          node.material.metalness = 0;
-          node.material.roughness = 1;
-          node.castShadow = false;
-          node.receiveShadow = false;
-          node.material.color = new THREE.Color(0xffffff);
-          node.material.transparent = true;
+          console.log("NOT MESH:", node.name);
         }
-      } else if (node.isMesh) {
-        node.material.metalness = 1;
-        node.material.roughness = 0.25;
-        console.log("MESH", node.name);
-      } else {
-        console.log("NOT MESH:", node.name);
-      }
+      });
+
+      scene.add(model);
+      const mixer = new THREE.AnimationMixer(model);
+      mixers.push(mixer);
+      const clips = gltf.animations;
+
+      clips.forEach((clip) => {
+        const action = mixer.clipAction(clip);
+        action.setLoop(THREE.LoopOnce);
+        action.clampWhenFinished = true;
+        actions.push(action);
+      });
+
+      renderer.render(scene, camera);
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar modelo ou texturas:", error);
     });
-
-    scene.add(model);
-    const mixer = new THREE.AnimationMixer(model);
-    mixers.push(mixer);
-    const clips = gltf.animations;
-
-    clips.forEach((clip) => {
-      const action = mixer.clipAction(clip);
-      action.setLoop(THREE.LoopOnce);
-      action.clampWhenFinished = true;
-      actions.push(action);
-    });
-
-    renderer.render(scene, camera);
-  }).catch((error) => {
-    console.error('Erro ao carregar modelo ou texturas:', error);
-  });
 }
